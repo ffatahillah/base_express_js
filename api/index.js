67837371a795c9431032e559f4ce903ba0fcb8cc
ffatahillah <94727287+ffatahillah@users.noreply.github.com>
2021-11-23 23:54:42 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '2133290397:AAHIgB1ZusJ-m2-TluhcnWMfZQNwfRPMk1U';
+const token = '2061338395:AAFNi402uA9CdkV7rpwejP_vwSyL0NRc5bU';
 const bot = new TelegramBot(token, {polling: true});
 
 
